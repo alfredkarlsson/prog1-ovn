d72225f7840a5e2b66a5edc54bd646b52a787690
alfredkarlsson <45688796+alfredkarlsson@users.noreply.github.com>
2020-09-28 13:34:08 +0200

    Add files via upload

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,5 @@
+fahrenheit = int(input("Enter temperature in degrees fahrenheit that you would like to convert: "))
+
+celsius = (fahrenheit -32) * 9/5
+
+print("the converted temperature is", celsius)
